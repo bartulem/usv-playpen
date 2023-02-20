a3e406cc5cd0426a032eed7113300b0e7bda87da
bartulem <mimica.bartul@gmail.com>
2023-02-20 01:03:39 -0500

    v0.1.0 include non-python files and img

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     ],
     keywords='neuroscience, mouse, usv, behavior',
     package_dir={'usv-playpen': 'src'},
-    package_data={'': ['/img/*.png', '*.css', '*.mplstyle']},
+    package_data={'': ['*.png', '*.css', '*.mplstyle']},
     include_package_data=True,
     python_requires="==3.10.*",
     description='GUI to conduct experiments w/ multichannel audio and video acquisition',
