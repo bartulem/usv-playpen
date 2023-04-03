2a43d3b21ce983e66d635ad9b8f4a0c49abfb28b
bartulem <mimica.bartul@gmail.com>
2023-04-03 14:13:56 -0400

    v0.1.2 change README and setup.py to current verison

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='usv-playpen',
-    version='0.1.1',
+    version='0.1.2',
     author='@bartulem',
     author_email='mimica.bartul@gmail.com',
     classifiers=[
