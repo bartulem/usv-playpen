03ae19b56fd91cd4254e41cf02d04db37180e4d4
bartulem <mimica.bartul@gmail.com>
2024-10-10 11:34:10 -0400

    v.0.5.0 added USV summary function w/ automated proofreading and notifications if there are no , added sorted everywhere for Linux, removed quantumrandom forever, added notifications for Ethernet connection changes, changed appearance of GUI to simplify processing

--- src/das_inference.py
+++ src/das_inference.py
@@ -159,122 +159,126 @@ class FindMouseVocalizations:
 
         session_id = self.root_directory.split(os.sep)[-1]
 
-        das_annotation_files = sorted(glob.glob(f"{self.root_directory}{os.sep}audio{os.sep}das_annotations{os.sep}*.csv"))
+        try:
+            das_annotation_files = sorted(glob.glob(f"{self.root_directory}{os.sep}audio{os.sep}das_annotations{os.sep}*.csv"))
 
-        # extract data from all CSV files
-        usv_data = {}
-        for one_file in das_annotation_files:
-            file_id = one_file.split(os.sep)[-1].split('_')[0] + '_' + one_file.split(os.sep)[-1].split('_')[2]
-            usv_data[file_id] = pd.read_csv(filepath_or_buffer=one_file, sep=',', index_col=0)
+            # extract data from all CSV files
+            usv_data = {}
+            for one_file in das_annotation_files:
+                file_id = one_file.split(os.sep)[-1].split('_')[0] + '_' + one_file.split(os.sep)[-1].split('_')[2]
+                usv_data[file_id] = pd.read_csv(filepath_or_buffer=one_file, sep=',', index_col=0)
 
-        # filter noise (usually the last row of every DAS file)
-        for channel_id, channel_data in usv_data.items():
-            usv_data[channel_id] = channel_data.loc[channel_data['name'] != 'noise']
+            # filter noise (usually the last row of every DAS file)
+            for channel_id, channel_data in usv_data.items():
+                usv_data[channel_id] = channel_data.loc[channel_data['name'] != 'noise']
 
-        usv_summary = pd.DataFrame.from_dict(data={'usv_id': [0.],
-                                                   'start': [0.],
-                                                   'stop': [0.],
-                                                   'duration': [0.],
-                                                   'peak_amp_ch': [0.],
-                                                   'mean_amp_ch': [0.],
-                                                   'chs_count': [0.],
-                                                   'chs_detected': [[0.]],
-                                                   'emitter': [np.nan]}, orient='columns')
+            usv_summary = pd.DataFrame.from_dict(data={'usv_id': [0.],
+                                                       'start': [0.],
+                                                       'stop': [0.],
+                                                       'duration': [0.],
+                                                       'peak_amp_ch': [0.],
+                                                       'mean_amp_ch': [0.],
+                                                       'chs_count': [0.],
+                                                       'chs_detected': [[0.]],
+                                                       'emitter': [np.nan]}, orient='columns')
 
-        usv_summary_null = usv_summary.copy()
+            usv_summary_null = usv_summary.copy()
 
-        # extract USV onsets and offsets, and channels they are detected on
-        progress_bar = tqdm(list(usv_data.keys()), desc="USV match search on {:s}".format(""), position=0, leave=True)
-        usv_num = 0
-        for channel_id in progress_bar:
-            progress_bar.set_description("USV match search on {:s}".format(channel_id), refresh=True)
-            channel_data = usv_data[channel_id]
-            if not channel_data.empty:
-                for index, row in channel_data.iterrows():
-                    try:
-                        next_idx_overlap_with_previous = pd.Interval(left=usv_summary.iloc[-1, 1],
-                                                                     right=usv_summary.iloc[-1, 2],
-                                                                     closed='neither').overlaps(pd.Interval(left=channel_data.loc[index, 'start_seconds'],
-                                                                                                            right=channel_data.loc[index, 'stop_seconds'],
-                                                                                                            closed='neither'))
-                    except (IndexError, KeyError):
-                        next_idx_overlap_with_previous = False
+            # extract USV onsets and offsets, and channels they are detected on
+            progress_bar = tqdm(list(usv_data.keys()), desc="USV match search on {:s}".format(""), position=0, leave=True)
+            usv_num = 0
+            for channel_id in progress_bar:
+                progress_bar.set_description("USV match search on {:s}".format(channel_id), refresh=True)
+                channel_data = usv_data[channel_id]
+                if not channel_data.empty:
+                    for index, row in channel_data.iterrows():
+                        try:
+                            next_idx_overlap_with_previous = pd.Interval(left=usv_summary.iloc[-1, 1],
+                                                                         right=usv_summary.iloc[-1, 2],
+                                                                         closed='neither').overlaps(pd.Interval(left=channel_data.loc[index, 'start_seconds'],
+                                                                                                                right=channel_data.loc[index, 'stop_seconds'],
+                                                                                                                closed='neither'))
+                        except (IndexError, KeyError):
+                            next_idx_overlap_with_previous = False
 
-                    if not next_idx_overlap_with_previous:
-                        if usv_num > 0:
-                            usv_summary = usv_summary._append(usv_summary_null, ignore_index=True)
-                        chs_count_temp = 1
-                        chs_detected_temp = [ch_conversion_dict[channel_id]]
-                        usv_summary.iloc[-1, 1] = row['start_seconds']
-                        usv_summary.iloc[-1, 2] = row['stop_seconds']
-                        usv_num += 1
-                        for channel_id_other, channel_data_other in usv_data.items():
-                            if channel_id != channel_id_other and not channel_data_other.empty:
-                                for index_other, row_other in channel_data_other.iterrows():
-                                    if pd.Interval(left=row['start_seconds'],
-                                                   right=row['stop_seconds'],
-                                                   closed='neither').overlaps(pd.Interval(left=row_other['start_seconds'],
-                                                                                          right=row_other['stop_seconds'],
-                                                                                          closed='neither')):
-                                        if row_other['start_seconds'] < usv_summary.iloc[-1, 1]:
-                                            usv_summary.iloc[-1, 1] = row_other['start_seconds']
-                                        if row_other['stop_seconds'] > usv_summary.iloc[-1, 2]:
-                                            usv_summary.iloc[-1, 2] = row_other['stop_seconds']
-                                        chs_count_temp += 1
-                                        chs_detected_temp.append(ch_conversion_dict[channel_id_other])
-                                        # the following segment checks for USV segments that are complete in some channels but broken in pieces in others
-                                        try:
-                                            if index_other + 1 <= channel_data_other.shape[0] and pd.Interval(left=row['start_seconds'],
-                                                                                                              right=row['stop_seconds'],
-                                                                                                              closed='neither').overlaps(pd.Interval(left=channel_data_other.loc[index_other + 1, 'start_seconds'],
-                                                                                                                                                     right=channel_data_other.loc[index_other + 1, 'stop_seconds'],
-                                                                                                                                                     closed='neither')):
-                                                channel_data_other.drop(labels=[index_other, index_other + 1], inplace=True)
-                                            else:
+                        if not next_idx_overlap_with_previous:
+                            if usv_num > 0:
+                                usv_summary = usv_summary._append(usv_summary_null, ignore_index=True)
+                            chs_count_temp = 1
+                            chs_detected_temp = [ch_conversion_dict[channel_id]]
+                            usv_summary.iloc[-1, 1] = row['start_seconds']
+                            usv_summary.iloc[-1, 2] = row['stop_seconds']
+                            usv_num += 1
+                            for channel_id_other, channel_data_other in usv_data.items():
+                                if channel_id != channel_id_other and not channel_data_other.empty:
+                                    for index_other, row_other in channel_data_other.iterrows():
+                                        if pd.Interval(left=row['start_seconds'],
+                                                       right=row['stop_seconds'],
+                                                       closed='neither').overlaps(pd.Interval(left=row_other['start_seconds'],
+                                                                                              right=row_other['stop_seconds'],
+                                                                                              closed='neither')):
+                                            if row_other['start_seconds'] < usv_summary.iloc[-1, 1]:
+                                                usv_summary.iloc[-1, 1] = row_other['start_seconds']
+                                            if row_other['stop_seconds'] > usv_summary.iloc[-1, 2]:
+                                                usv_summary.iloc[-1, 2] = row_other['stop_seconds']
+                                            chs_count_temp += 1
+                                            chs_detected_temp.append(ch_conversion_dict[channel_id_other])
+                                            # the following segment checks for USV segments that are complete in some channels but broken in pieces in others
+                                            try:
+                                                if index_other + 1 <= channel_data_other.shape[0] and pd.Interval(left=row['start_seconds'],
+                                                                                                                  right=row['stop_seconds'],
+                                                                                                                  closed='neither').overlaps(pd.Interval(left=channel_data_other.loc[index_other + 1, 'start_seconds'],
+                                                                                                                                                         right=channel_data_other.loc[index_other + 1, 'stop_seconds'],
+                                                                                                                                                         closed='neither')):
+                                                    channel_data_other.drop(labels=[index_other, index_other + 1], inplace=True)
+                                                else:
+                                                    channel_data_other.drop(labels=index_other, inplace=True)
+                                            except (IndexError, KeyError):
                                                 channel_data_other.drop(labels=index_other, inplace=True)
-                                        except (IndexError, KeyError):
-                                            channel_data_other.drop(labels=index_other, inplace=True)
 
-                                        break
+                                            break
 
-                        channel_data.drop(labels=index, inplace=True)
+                            channel_data.drop(labels=index, inplace=True)
 
-                        usv_summary.iloc[-1, 6] = chs_count_temp
-                        usv_summary.iat[-1, 7] = chs_detected_temp
+                            usv_summary.iloc[-1, 6] = chs_count_temp
+                            usv_summary.iat[-1, 7] = chs_detected_temp
 
-        # compute USV durations and order them by start time
-        usv_summary['duration'] = usv_summary['stop'] - usv_summary['start']
-        usv_summary.sort_values(by='start', ascending=True, inplace=True)
+            # compute USV durations and order them by start time
+            usv_summary['duration'] = usv_summary['stop'] - usv_summary['start']
+            usv_summary.sort_values(by='start', ascending=True, inplace=True)
 
-        # find peak and mean amplitude channels
-        audio_file_loc = sorted(glob.glob(f"{self.root_directory}{os.sep}audio{os.sep}hpss_filtered{os.sep}*.mmap"))[0]
-        audio_file_name = os.path.basename(audio_file_loc)
-        data_type, channel_num, sample_num, audio_sampling_rate = audio_file_name.split("_")[-1][:-5], int(audio_file_name.split("_")[-2]), int(audio_file_name.split("_")[-3]), int(audio_file_name.split("_")[-4])
-        audio_file_data = np.memmap(filename=audio_file_loc, mode='r', dtype=data_type, shape=(sample_num, channel_num))
-        for index, row in tqdm(usv_summary.iterrows(), desc="USV clean-up progress", total=usv_summary.shape[0], position=0, leave=True):
-            start_usv = int(np.floor(row['start'] * audio_sampling_rate))
-            stop_usv = int(np.ceil(row['stop'] * audio_sampling_rate))
-            peak_amp_ch = np.unravel_index(np.argmax(audio_file_data[start_usv:stop_usv, :]), audio_file_data.shape)[1]
-            mean_amp_ch = np.argmax(np.abs(audio_file_data[start_usv:stop_usv, :]).mean(axis=0))
-            usv_detected_chs = list(row['chs_detected'])
+            # find peak and mean amplitude channels
+            audio_file_loc = sorted(glob.glob(f"{self.root_directory}{os.sep}audio{os.sep}hpss_filtered{os.sep}*.mmap"))[0]
+            audio_file_name = os.path.basename(audio_file_loc)
+            data_type, channel_num, sample_num, audio_sampling_rate = audio_file_name.split("_")[-1][:-5], int(audio_file_name.split("_")[-2]), int(audio_file_name.split("_")[-3]), int(audio_file_name.split("_")[-4])
+            audio_file_data = np.memmap(filename=audio_file_loc, mode='r', dtype=data_type, shape=(sample_num, channel_num))
+            for index, row in tqdm(usv_summary.iterrows(), desc="USV clean-up progress", total=usv_summary.shape[0], position=0, leave=True):
+                start_usv = int(np.floor(row['start'] * audio_sampling_rate))
+                stop_usv = int(np.ceil(row['stop'] * audio_sampling_rate))
+                peak_amp_ch = np.unravel_index(np.argmax(audio_file_data[start_usv:stop_usv, :]), audio_file_data.shape)[1]
+                mean_amp_ch = np.argmax(np.abs(audio_file_data[start_usv:stop_usv, :]).mean(axis=0))
+                usv_detected_chs = list(row['chs_detected'])
 
-            # remove USV segments if they appear only on one channel; this gets rid of some true positives, but false positives to a larger extent
-            condition_1 = len(usv_detected_chs) < 2
-            # remove USV segments if they don't appear on both peak and mean amplitude channels; this is clearly noise
-            condition_2 = peak_amp_ch not in usv_detected_chs or mean_amp_ch not in usv_detected_chs
-            # if the USV is detected on two channels, but the peak amplitude channel is not the same as the mean amplitude channel, it is likely noise
-            condition_3 = len(usv_detected_chs) == 2 and peak_amp_ch != mean_amp_ch
+                # remove USV segments if they appear only on one channel; this gets rid of some true positives, but false positives to a larger extent
+                condition_1 = len(usv_detected_chs) < 2
+                # remove USV segments if they don't appear on both peak and mean amplitude channels; this is clearly noise
+                condition_2 = peak_amp_ch not in usv_detected_chs or mean_amp_ch not in usv_detected_chs
+                # if the USV is detected on two channels, but the peak amplitude channel is not the same as the mean amplitude channel, it is likely noise
+                condition_3 = len(usv_detected_chs) == 2 and peak_amp_ch != mean_amp_ch
 
-            if condition_1 or condition_2 or condition_3:
-                usv_summary.drop(labels=index, inplace=True)
-            else:
-                usv_summary.at[index, 'peak_amp_ch'] = peak_amp_ch
-                usv_summary.at[index, 'mean_amp_ch'] = mean_amp_ch
+                if condition_1 or condition_2 or condition_3:
+                    usv_summary.drop(labels=index, inplace=True)
+                else:
+                    usv_summary.at[index, 'peak_amp_ch'] = peak_amp_ch
+                    usv_summary.at[index, 'mean_amp_ch'] = mean_amp_ch
 
-        # give ID number to each USV
-        usv_summary['usv_id'] = [f"{_num:04d}" for _num in range(usv_summary.shape[0])]
+            # give ID number to each USV
+            usv_summary['usv_id'] = [f"{_num:04d}" for _num in range(usv_summary.shape[0])]
 
-        # save summary file
-        usv_summary.to_csv(path_or_buf=f"{self.root_directory}{os.sep}audio{os.sep}{session_id}_usv_summary.csv",
-                           sep=',',
-                           index=False)
+            # save summary file
+            usv_summary.to_csv(path_or_buf=f"{self.root_directory}{os.sep}audio{os.sep}{session_id}_usv_summary.csv",
+                               sep=',',
+                               index=False)
+
+        except (IndexError, FileNotFoundError):
+            self.message_output(f"No DAS annotations found in directory: {self.root_directory}. Skipping summary generation.")
