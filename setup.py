f722fbeca4e7fa6878a1eced8df3ee53d522921f
bartulem <mimica.bartul@gmail.com>
2024-12-02 19:22:53 -0500

    v0.7.3 (1) updated synchronize_files to loop through pos and neg events and eliminate close ones, (2) add username on title page, (3) install package through conda and start it with simple command

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ setup(
         'Programming Language :: Python :: 3 :: Only',
     ],
     keywords='neuroscience, mouse, usv, behavior',
-    packages=['usv_playpen'],
-    package_dir={'usv_playpen': 'usv-playpen'},
+    packages=['usv-playpen'],
+    package_dir={'usv-playpen': 'src'},
     package_data={'usv-playpen': ['*.png', '*.css', '*.mplstyle', '*.ttf']},
     include_package_data=True,
     python_requires="==3.10.*",
@@ -33,7 +33,7 @@ setup(
     license='MIT',
     entry_points={
         'console_scripts': [
-            'usv-playpen = usv_playpen.src.usv_playpen_gui:main'
+            'usv-playpen = usv-playpen.src.usv_playpen_gui:main'
         ]
     },
     install_requires=['av==10.0.0',
