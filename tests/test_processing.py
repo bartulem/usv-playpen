4b04235753eb68f567bc927b0187ea9412f8d539
David Turner <dmturner@princeton.edu>
2025-06-18 13:59:00 -0400

    Convert tests to pytest.
    - Pytest is generally better than unittest.
    - Commented out email sending tests for now, if this actually sends an email this is probably something you don't want to do in unit tests. If one really wants to test this there are probably better ways.

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -8,79 +8,67 @@ from __future__ import annotations
 import os
 import pathlib
 import subprocess
-import sys
-import unittest
 
 import toml
 
-from src.send_email import Messenger
+from usv_playpen.send_email import Messenger
+
+if os.name == "nt":
+    command_addition = "cmd /c "
+    shell_usage_bool = False
+else:
+    command_addition = ""
+    shell_usage_bool = True
+
+config_dir_global = pathlib.Path(__file__).parent.parent / "src" / "_config"
+esd = toml.load(f"{config_dir_global}{os.sep}behavioral_experiments_settings.toml")
 
 
-class TestProcessing(unittest.TestCase):
-    if os.name == "nt":
-        command_addition = "cmd /c "
-        shell_usage_bool = False
-    else:
-        command_addition = ""
-        shell_usage_bool = True
-
-    config_dir_global = pathlib.Path(__file__).parent.parent / "src" / "_config"
-
-    esd = toml.load(f"{config_dir_global}{os.sep}behavioral_experiments_settings.toml")
-
-    def test_send_email(self):
-        try:
-            email_receiver = [str(self.email_address)]
-        except IndexError:
-            print("Error: Missing e-mail argument. Please provide it.")
-
-        # test email sending
-        try:
-            Messenger(
-                receivers=email_receiver, exp_settings_dict=self.esd
-            ).send_message(
-                subject="Test",
-                message="This is a 165B processing test email. Please do not reply.",
-            )
-            email_success = True
-        except Exception:
-            email_success = False
-
-        self.assertTrue(expr=email_success, msg="165B not able to send e-mails.")
-
-    def test_video_processing(self):
-        # test video processing capabilities
-        try:
-            subprocess.Popen(
-                args=f"""{self.command_addition}ffmpeg -version""",
-                shell=self.shell_usage_bool,
-            ).wait()
-            video_processing_success = True
-        except subprocess.CalledProcessError as e:
-            print(e)
-            video_processing_success = False
-
-        self.assertTrue(
-            expr=video_processing_success,
-            msg="FFMPEG not functional from command line.",
-        )
-
-    def test_audio_processing(self):
-        try:
-            subprocess.Popen(
-                args=f"""{self.command_addition}sox --version""",
-                shell=self.shell_usage_bool,
-            ).wait()
-            audio_processing_success = True
-        except subprocess.CalledProcessError as e:
-            print(e)
-            audio_processing_success = False
-
-        self.assertTrue(
-            expr=audio_processing_success, msg="SOX not functional from command line."
-        )
+# def test_send_email():
+#     try:
+#         email_receiver = [str(email_address)]
+#     except IndexError:
+#         print("Error: Missing e-mail argument. Please provide it.")
+#
+#     # test email sending
+#     try:
+#         Messenger(
+#             receivers=email_receiver, exp_settings_dict=esd
+#         ).send_message(
+#             subject="Test",
+#             message="This is a 165B processing test email. Please do not reply.",
+#         )
+#         email_success = True
+#     except Exception:
+#         email_success = False
+#
+#     assert email_success, "165B not able to send e-mails."
 
 
-if __name__ == "__main__":
-    TestProcessing.email_address = sys.argv.pop()
-    unittest.main()
+def test_video_processing():
+    # test video processing capabilities
+    try:
+        subprocess.Popen(
+            args=f"""{command_addition}ffmpeg -version""",
+            shell=shell_usage_bool,
+        ).wait()
+        video_processing_success = True
+    except subprocess.CalledProcessError as e:
+        print(e)
+        video_processing_success = False
+
+    assert video_processing_success, "FFMPEG not functional from command line."
+
+
+def test_audio_processing():
+    try:
+        subprocess.Popen(
+            args=f"""{command_addition}sox --version""",
+            shell=shell_usage_bool,
+        ).wait()
+        audio_processing_success = True
+    except subprocess.CalledProcessError as e:
+        print(e)
+        audio_processing_success = False
+
+    assert audio_processing_success, "SOX not functional from command line."
--- tests/test_recording.py
+++ tests/test_recording.py
@@ -9,74 +9,66 @@ import configparser
 import os
 import pathlib
 import sys
-import unittest
 
 import motifapi
 import toml
 
-from src.send_email import Messenger
+from usv_playpen.send_email import Messenger
+
+config_dir_global = pathlib.Path(__file__).parent.parent / "src" / "_config"
+exp_settings_dict = toml.load(
+    f"{config_dir_global}{os.sep}behavioral_experiments_settings.toml"
+)
 
 
-class TestRecording(unittest.TestCase):
-    config_dir_global = pathlib.Path(__file__).parent.parent / "src" / "_config"
+# def test_recording_send_email():
+#     try:
+#         email_receiver = [str(email_address)]
+#     except IndexError:
+#         print("Error: Missing e-mail argument. Please provide it.")
+#
+#     # test email sending
+#     try:
+#         Messenger(
+#             receivers=email_receiver, exp_settings_dict=exp_settings_dict
+#         ).send_message(
+#             subject="Test",
+#             message="This is a 165B recording test email. Please do not reply.",
+#         )
+#         email_success = True
+#     except Exception:
+#         email_success = False
+#
+#     assert email_success, "165B not able to send e-mails"
 
-    exp_settings_dict = toml.load(
-        f"{config_dir_global}{os.sep}behavioral_experiments_settings.toml"
+
+def test_recording_video_software():
+    # test video recording capabilities
+    video_config = configparser.ConfigParser()
+    video_config.read(f"{config_dir_global}{os.sep}motif_config.ini")
+
+    try:
+        motif_api = motifapi.MotifApi(
+            video_config["motif"]["master_ip_address"], video_config["motif"]["api"]
+        )
+        motif_success = True
+    except motifapi.api.MotifError:
+        motif_success = False
+
+    assert motif_success, "Motif not operational on this PC."
+
+
+def test_recording_audio_sync_software():
+    # test audio / sync_software presence
+    config_file_status = os.path.isfile(
+        f"{config_dir_global}{os.sep}avisoft_config.ini"
+    ) and os.path.isfile(f"{config_dir_global}{os.sep}coolterm_config.stc")
+    assert config_file_status, "Audio/sync software config files missing."
+
+    software_status = os.path.isfile(
+        f"{exp_settings_dict['avisoft_recorder_exe']}{os.sep}rec_usgh.exe"
+    ) and os.path.isfile(
+        f"{exp_settings_dict['coolterm_basedirectory']}{os.sep}CoolTerm.exe"
     )
 
-    def test_recording_send_email(self):
-        try:
-            email_receiver = [str(self.email_address)]
-        except IndexError:
-            print("Error: Missing e-mail argument. Please provide it.")
-
-        # test email sending
-        try:
-            Messenger(
-                receivers=email_receiver, exp_settings_dict=self.exp_settings_dict
-            ).send_message(
-                subject="Test",
-                message="This is a 165B recording test email. Please do not reply.",
-            )
-            email_success = True
-        except Exception:
-            email_success = False
-
-        self.assertTrue(expr=email_success, msg="165B not able to send e-mails")
-
-    def test_recording_video_software(self):
-        # test video recording capabilities
-        video_config = configparser.ConfigParser()
-        video_config.read(f"{self.config_dir_global}{os.sep}motif_config.ini")
-
-        try:
-            self.motif_api = motifapi.MotifApi(
-                video_config["motif"]["master_ip_address"], video_config["motif"]["api"]
-            )
-            motif_success = True
-        except motifapi.api.MotifError:
-            motif_success = False
-
-        self.assertTrue(expr=motif_success, msg="Motif not operational on this PC.")
-
-    def test_recording_audio_sync_software(self):
-        # test audio / sync_software presence
-        config_file_status = os.path.isfile(
-            f"{self.config_dir_global}{os.sep}avisoft_config.ini"
-        ) and os.path.isfile(f"{self.config_dir_global}{os.sep}coolterm_config.stc")
-
-        software_status = os.path.isfile(
-            f"{self.exp_settings_dict['avisoft_recorder_exe']}{os.sep}rec_usgh.exe"
-        ) and os.path.isfile(
-            f"{self.exp_settings_dict['coolterm_basedirectory']}{os.sep}CoolTerm.exe"
-        )
-
-        self.assertTrue(
-            expr=config_file_status and software_status,
-            msg="Audio / SYNC software and/or config files not ready for use.",
-        )
-
-
-if __name__ == "__main__":
-    TestRecording.email_address = sys.argv.pop()
-    unittest.main()
+    assert software_status, "Audio / SYNC software not ready for use."
