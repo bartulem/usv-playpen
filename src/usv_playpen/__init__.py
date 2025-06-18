521e0ff1670d08b1f52a00c655bd4b50a0af05b6
David Turner <dmturner@princeton.edu>
2025-06-18 12:20:21 -0400

    Add a good default __init__.py

---
+++ src/usv_playpen/__init__.py
@@ -0,0 +1,11 @@
+"""
+Copyright (c) 2025 Bartul Mimica. All rights reserved.
+
+usv_playpen: GUI/CLI to conduct, process, and analyze experiments w/ multichannel e-phys, audio, and video acquisition.
+"""
+
+from __future__ import annotations
+
+from ._version import version as __version__
+
+__all__ = ["__version__"]
