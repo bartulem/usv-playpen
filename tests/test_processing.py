63ddebebc212503a13d59bf64a9868bf0caf67dd
David Turner <dmturner@princeton.edu>
2025-08-21 20:56:58 -0400

    Add shell=True to Popen calls.
    Hopefully this will fix binary path issues on MacOS and Linux.

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -9,12 +9,12 @@ import subprocess
 
 
 def test_video_processing():
-    assert subprocess.Popen(args="ffmpeg -version").wait() == 0, (
+    assert subprocess.Popen(args="ffmpeg -version", shell=True).wait() == 0, (
         "FFMPEG not functional from command line."
     )
 
 
 def test_audio_processing():
-    assert subprocess.Popen(args="static_sox --version").wait() == 0, (
+    assert subprocess.Popen(args="static_sox --version", shell=True).wait() == 0, (
         "SOX not functional from command line."
     )
