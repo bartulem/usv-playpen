eb5b2f3b0adb6a6d8afb836a27679cd7a2a64b7d
bartulem <mimica.bartul@gmail.com>
2025-08-25 21:55:29 -0400

    v0.8.10 updates: (1) added dropouts in audio_triggerbox_sync_info file, (2) fixed analyses CLI input dictionary, (3) added function to create naturalistic playback files, (4) aded GPU-support for video file writer

--- tests/test_package (2).py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-import importlib.metadata
-
-import usv_playpen as m
-
-
-def test_version():
-    assert importlib.metadata.version("usv_playpen") == m.__version__
